b8c71b0e66433e84b3c13e6c12cb9c4bd89b14cd
guocz <2589945540@qq.com>
2023-10-24 14:10:37 +0800

    SQLite数据库添加数据

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -13,8 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new MyDatabaseHelper(this, "BookStore.dp", null, 2);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.dp", null, 3);
         Button createDataBase = findViewById(R.id.create_database);
+        Button addData = findViewById(R.id.add_data);
+        addData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("name", "peace");
+            values.put("author", "mike");
+            values.put("price", 16.98);
+            values.put("pages", 233);
+            db.insert("Book", null, values);
+
+            values.put("name", "love");
+            values.put("author", "mike");
+            values.put("price", 16.98);
+            values.put("pages", 233);
+            db.insert("Book", null, values);
+        });
         createDataBase.setOnClickListener(v -> {
         dbHelper.getWritableDatabase();
         });
