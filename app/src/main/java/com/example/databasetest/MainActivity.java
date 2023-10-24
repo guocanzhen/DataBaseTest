8343c7a90d152cf1c59a32df66b378b6f92b02ff
guocz <2589945540@qq.com>
2023-10-24 16:07:28 +0800

    SQLite数据库查询数据

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         Button addData = findViewById(R.id.add_data);
         Button updateData = findViewById(R.id.update_data);
         Button deleteData = findViewById(R.id.delete_data);
+        Button queryData = findViewById(R.id.query_data);
+        queryData.setOnClickListener(v -> {
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            Cursor cursor = db.query("Book", null, null, null, null, null, null);
+            if (cursor.moveToFirst()) {
+                do {
+                    String name = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                    String author = cursor.getString(cursor.getColumnIndexOrThrow("author"));
+                    double price = cursor.getDouble(cursor.getColumnIndexOrThrow("price"));
+                    int pages = cursor.getInt(cursor.getColumnIndexOrThrow("pages"));
+                    Toast.makeText(this,"book name is " + name, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"book author is " + author, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"book price is " + price, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,"book pages is " + pages, Toast.LENGTH_SHORT).show();
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        });
         deleteData.setOnClickListener(v -> {
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             db.delete("Book", "price > ?", new String[]{"11"});
